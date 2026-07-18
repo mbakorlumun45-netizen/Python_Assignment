436d5b8444f1b9c4966195e8e1edfbdd22e1cae3
mbakorlumun45-netizen <mbakorlumun45@gmail.com>
2026-07-18 19:58:15 +0100

    Add Orion afterparty folder

---
+++ Orion afterparty/LetterGrade.py
@@ -0,0 +1,36 @@
+#collect input from user five times import java.util.Scanner;
+#check average sum divide by frequency
+#assign grade to each number of it 
+#condition each grade to their letter grades
+#print the average associated with each grade
+
+	
+score1 = print("Enter first score")
+score2 = print("Enter second score")
+score3 = print("Enter third score")
+
+sum = 0
+grade = 0
+	
+grade = score1 + score2 + score3
+	
+sum = grade = 0
+	
+grade = sum / 3
+	
+if(grade > 90 and grade <= 100):
+	print("A")
+else if(grade > 80 and grade <= 90):
+	print("B")
+else if(grade > 70 and grade <= 80):
+	print("C")
+else if(grade > 60 and grade <= 70):
+	print("D")
+else if(grade < 60 and grade):
+	print("F")
+	print("Repeat the course")
+	
+	
+	print(grade)
+	
+ 
